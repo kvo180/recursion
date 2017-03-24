2b62875420b84645386b8445c83a40274e4eefc2
kvo180 <khoavo180@gmail.com>
2017-03-23 21:18:11 -0700

    Add working code for stringifyJSON.js
    All tests passed

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,35 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
-};
+
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (obj === null) {
+    return 'null';
+  } else if (Array.isArray(obj)) {
+    var arrStr = [];
+    if (obj.length === 0 || obj[0] === undefined) {
+      return '[]';
+    } else {
+      for (var i = 0; i < obj.length; i++) {
+        arrStr.push(stringifyJSON(obj[i]));
+      }
+      return '[' + arrStr + ']';
+    }
+  } else if (typeof obj === 'object') {
+    var objStr = [];
+    if (Object.keys(obj).length === 0) {
+      return '{}';
+    } else {
+      for (var key in obj) {
+      	if (obj[key] === undefined || typeof obj[key] === 'function') {
+      		return '{}';
+      	}
+        objStr.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+      }
+      return '{' + objStr + '}';
+    }
+  }  else {
+    return String(obj);
+  }
+}
